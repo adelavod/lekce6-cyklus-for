e107807e3d64a5b16c4a7a6c8c3fe9190fd940f4
Adéla Krásová <>
2022-03-27 10:29:22 +0200

    homework: WHILE cycle task finished

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 // Použij výpis do konzole prohlížeče pomocí console.log('text')
 // Konzoli otevřeš v Chrome stiskem F12 (na Windows) nebo Command-Option-I (na Macu)
-console.log("DEVELOPPP");
+console.log("DEVELOP BRANCH");
 
 // První příklad - vypiš vzestupně čísla od 0 do 10
 console.log('První příklad - vzestupná řada 0 - 10');
 
 //TADY NAPIS CYKLUS
-for (let i=0; i<=10; i++) {
-    console.log(i);
-}
+let i = 0;
+while (i<=10) {
+    console.log(i)
+    i++;
+};
 
 // Druhý příklad - zkopíruj předchozí příklad
 // a vyzkoušej, jaký je rozdíl mezi <= a <
@@ -17,19 +19,23 @@ for (let i=0; i<=10; i++) {
 console.log('Druhý příklad - rozdíl mezi <= a <');
 
 //TADY NAPIS CYKLUS
-
-for (let i=0; i<=10; i++) {
-    console.log(i);
+let j = 0;
+while (j<=10) {
+    console.log(j)
+    j++;
 };
-for (let i=0; i<10; i++) {
-    console.log(i);
+let k=0;
+while (k<10) {
+    console.log(k)
+    k++;
 };
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
 console.log('Třetí příklad - sestupná řada 10 - 0');
 
 //TADY NAPIS CYKLUS
-
-for (let i=10; i>=0; i--) {
-    console.log(i);
-};
+let l=10;
+while (l>=0){
+    console.log(l);
+    l--;
+}
